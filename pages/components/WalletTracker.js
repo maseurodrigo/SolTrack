986cb0e25303d92d5ec632c1b4518880d9f0c0d2
Rodrigo Tavares <rodrigo.tavares@wetrack.pt>
2025-01-12 03:08:25 +0000

    Railway > Fixing last commit export components/PlatformRadio.js 2/2

--- pages/components/WalletTracker.js
+++ pages/components/WalletTracker.js
@@ -5,9 +5,27 @@ import NumberFlow, { NumberFlowGroup } from '@number-flow/react';
 import Image from 'next/image';
 
 import RemoveBackCSS from './RemoveBackCSS';
-import PlatformRadio from './PlatformRadio';
 import AnimatedBorderTrail from './animata/container/animated-border-trail.tsx';
 import { calcPnLPerc } from "/utils/calcPnLPercentage";
+
+export const PlatformRadio = (props) => {
+  const {children, ...otherProps} = props;
+  return (
+    <Radio
+      {...otherProps}
+      classNames={{
+        base: cn(
+          "flex justify-center items-center",
+          "rounded-lg bg-[#1F2029] border border-[#343641]",
+          "px-4 py-2 max-w-[300px] transition-all duration-200 cursor-pointer",
+          "data-[selected=true]:outline-none data-[selected=true]:ring-1 data-[selected=true]:ring-opacity-50 data-[selected=true]:ring-green-800 data-[selected=true]:shadow-lg"
+        ),
+      }}
+    >
+      {children}
+    </Radio>
+  );
+};
 
 const WalletTracker = () => {
   const [walletData, setWalletData] = useState(null);
